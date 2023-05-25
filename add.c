7722832bc230c1d3b45a306609b2b3171bdf9f9c
Ahemd <ahmedsaye23156@gmail.com>
2023-05-26 00:07:07 +0300

    trying other code

--- add.c
+++ add.c
@@ -1,17 +1,52 @@
+// #include "monty.h"
+// /**
+//  * f_add - adds the top two elements of the stack.
+//  * @head: stack head
+//  * @counter: line_number
+//  * Return: no return
+// */
+// void add(stack_t **stack, unsigned int line_number)
+// {
+// 	if (*stack == NULL || (*stack)->next == NULL)
+// 	{
+// 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	(*stack)->next->n += (*stack)->n;
+// 	_pop(stack, line_number);
+// }
+
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
+
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void add(stack_t **stack, unsigned int line_number)
+ * _add - adds the first two nodes of the stack
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
+ */
+void _add(stack_t **stack, unsigned int line_cnt)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	(*stack)->next->n += (*stack)->n;
-	_pop(stack, line_number);
+    int result;
+
+    // Checking if the stack is empty or contains only one element
+    if (!stack || !*stack || !((*stack)->next))
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
+        exit(EXIT_FAILURE);
+    }
+
+    // Performing the addition operation
+    result = ((*stack)->next->n) + ((*stack)->n);
+
+    // Removing the top node from the stack
+    pop(stack, line_cnt); /* For top node */
+
+    // Assigning the result to the new top node
+    (*stack)->n = result;
 }
--- div.c
+++ div.c
@@ -1,22 +1,65 @@
+// #include "monty.h"
+
+// /**
+//  * div - divide top of stack and second top of stack
+//  * @stack: pointer to linked list stack
+//  * @line_number: number of line opcode occurs on
+//  */
+// void _div(stack_t **stack, unsigned int line_number)
+// {
+// 	if (*stack == NULL || (*stack)->next == NULL)
+// 	{
+// 		fprintf(stderr,"L%d: can't div, stack too short\n", line_number);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	if ((*stack)->n == 0)
+// 	{
+// 		fprintf(stderr,"L%d: division by zero\n", line_number);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	(*stack)->next->n /= (*stack)->n;
+// 	_pop(stack, line_number);
+// }
+
+
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
 
 /**
- * div - divide top of stack and second top of stack
- * @stack: pointer to linked list stack
- * @line_number: number of line opcode occurs on
+ * _div - divides the next top value by the top value
+ * @stack: stack given by main
+ * @line_cnt: line counter
+ *
+ * Return: void
  */
-void _div(stack_t **stack, unsigned int line_number)
+void _div(stack_t **stack, unsigned int line_cnt)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr,"L%d: can't div, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	if ((*stack)->n == 0)
-	{
-		fprintf(stderr,"L%d: division by zero\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	(*stack)->next->n /= (*stack)->n;
-	_pop(stack, line_number);
+    int result;
+
+    // Checking if the stack is empty or contains only one element
+    if (!stack || !*stack || !((*stack)->next))
+    {
+        fprintf(stderr, "L%d: can't div, stack too short\n", line_cnt);
+        exit(EXIT_FAILURE);
+    }
+
+    // Checking if division by zero is attempted
+    if (((*stack)->n) == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line_cnt);
+        exit(EXIT_FAILURE);
+    }
+
+    // Performing the division operation
+    result = ((*stack)->next->n) / ((*stack)->n);
+
+    // Removing the top node from the stack
+    pop(stack, line_cnt); /* For top node */
+
+    // Assigning the result to the new top node
+    (*stack)->n = result;
 }

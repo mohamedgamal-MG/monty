1ef7d8443dae7c7d874cc29df0188e3103a59f4d
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-05-25 05:30:58 -0700

    monty

--- monty.h
+++ monty.h
@@ -35,4 +35,35 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void add(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+instruct_func get_func(char *str);
+stack_t *add_node(stack_t **head, const int n);
+int delete_node(stack_t **head, unsigned int index);
+stack_t *add_node_end(stack_t **head, const int n);
+void free_list(stack_t *head);
+int main(int ac, char **av);
+int sq_flag = 0;
+void _mod(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _nop(__attribute__ ((unused))stack_t **stack,
+	  __attribute__ ((unused))unsigned int line_number);
+void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
+char *parse(char *line);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void read_file(char *filename, stack_t **stack);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
+void stack(__attribute__ ((unused)) stack_t **stack,
+	    __attribute__ ((unused)) unsigned int line_number);
+void queue(__attribute__ ((unused))stack_t **stack,
+	    __attribute__ ((unused))unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void error_exit(stack_t **stack);
+int isnumber(char *str);
+void _rotl(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
+void _rotr(stack_t **stack, __attribute__ ((unused))unsigned int line_number);
 
